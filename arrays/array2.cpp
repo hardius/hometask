af11693b0fd1b68e79ccfeab3cc6cccb71695243
hardius <113363945+hardius@users.noreply.github.com>
2022-11-10 21:55:23 +0300

    Add files via upload

---
+++ arrays/array2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    int N;
+    int a = new int[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> a[i];
+    }
+
+    int i = 0, j = N;
+
+    while (i <= N / 2)
+    {
+        std::cout << a[i] << std::endl;
+        std::cout << a[j] << std::endl;
+
+        ++i;
+        --j
+    }
+    return 0;
+}
