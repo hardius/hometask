8553c8f45736b53927e7f5f110d7fb0214a9683f
hardius <113363945+hardius@users.noreply.github.com>
2022-11-12 14:07:08 +0300

    Update and rename array.cpp to array6.cpp

---
+++ arrays/array6.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using std::cin;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int* arr = new int[N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+	}
+
+	bool IsProgression = true;
+	int d1, d2;
+	if (N > 1)
+	{
+		d1 = arr[1] - arr[0];
+	}
+	else
+	{
+		return 0;
+	}
+
+	for (int i = 1; i < N - 1; ++i)
+	{
+		d2 = arr[i + 1] - arr[i];
+		if (d1 == d2 && d1 != 0)
+		{
+			d1 = d2;
+		}
+		else
+		{
+			IsProgression = false;
+			break;
+		}
+	}
+
+	if (IsProgression)
+	{
+		return d1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
