265b4eda6291b949688706a05527b1483f07e4e4
hardius <113363945+hardius@users.noreply.github.com>
2022-11-12 12:14:45 +0300

    Add files via upload

---
+++ sail.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+
+bool figure 1(double x, double y) {
+    return (x >= 0 && y >= 0 && y <= -2 * (x - 1));
+}
+
+bool figure2(double x, double y) {
+    return (x <= 0 && y >= 0 && y <= 2 * (x + 1));
+}
+
+bool figure3(double x, double y) {
+    return (x <= 0 && y <= 0 && y >= -sqrt(1 - (x + 1) * (x + 1));
+}
+
+bool figure4(double x, double y) {
+    return (x >= 0 && y <= 0 && y >= -sqrt(1 - (x - 1) * (x - 1));
+}
+
+int main() 
+{
+    double x, y;
+
+    cin >> x;
+    cin >> y;
+
+    if (figure1(x, y) || figure2(x, y) || figure3(x, y) || figure4(x, y))
+    {
+        cout << "Inside sail\n";
+    }
+    else
+    {
+        cout << "Out of sail\n";
+    }
+}
