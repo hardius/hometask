769fcfc08230b64b36d3af3670c677ab60c6a105
hardius <113363945+hardius@users.noreply.github.com>
2022-11-14 01:19:00 +0300

    Update misc_cpp7.cpp

--- misc_cpp/misc_cpp7.cpp
+++ misc_cpp/misc_cpp7.cpp
@@ -6,15 +6,15 @@ int main()
     double R = 1e6;
     long long N = 0;
 
-    for (int x = -R; x <= R; ++x)
+    for (int x = 0; x <= R; ++x)
     {
-        for (int y = -R; y <= R; ++y)
+        for (int y = 0; y <= R; ++y)
             if (x * x + y * y <= R * R)
             {
                 ++N;
             }
     }
 
-    std::cout << N;
+    std::cout << 4*N;
     return 0;
 }
