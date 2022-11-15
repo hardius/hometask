e020c643e14e85b2dc98e570ad328c45c52b8412
hardius <113363945+hardius@users.noreply.github.com>
2022-11-15 19:35:58 +0300

    Add files via upload

---
+++ debts/reversestrings.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <cmath>
+
+std::string reverseword(std::string word)
+{
+    int i = 0, n = word.length(), j = n - 1;
+    char temp;
+
+    n = n / 2;
+    for (; i < n; ++i)
+    {
+        temp = word[i];
+        word[i] = word[j];
+        word[j] = temp;
+        
+        --j;
+    }
+
+    return word;
+}
+
+void reverseword2(std::string &word)
+{
+    int i = 0, n = word.length(), j = n - 1;
+    char temp;
+
+    n = n / 2;
+    for (; i < n; ++i)
+    {
+        temp = word[i];
+        word[i] = word[j];
+        word[j] = temp;
+
+        --j;
+    }
+}
+
+std::string reversesentence(std::string sentence)
+{
+    int n = sentence.length(), mark = 0;
+    char temp;
+
+    for (int i = 0; ; ++i)
+    {
+        if (sentence[i] == ' ' || i == n - 1)
+        {
+            int k = (i == n - 1) ? i : i - 1;
+            for (int j = mark; j < mark + (i - mark) / 2; ++j)
+            {
+                temp = sentence[j];
+                sentence[j] = sentence[k];
+                sentence[k] = temp;
+
+                k--;
+            }
+            if (i == n - 1)
+            {
+                break;
+            }
+            mark = i + 1;
+        }
+    }
+    return sentence;
+}
+
+int main()
+{
+    std::cout << reverseword("hello") << std::endl;
+    std::cout << reversesentence("This is an example!") << std::endl;
+}
