ecd43e29b3e0b8ad60533b9f8f285cca62e8ae8b
hardius <113363945+hardius@users.noreply.github.com>
2022-11-15 19:16:49 +0300

    Add files via upload

---
+++ debts/misc_cpp7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double R = 3;
+    long long Nsqr = (R + 1) * (R + 1), N = 0;
+
+    for (int x = 1; x <= R; ++x)
+    {
+        for (int y = R; ; --y)
+        {
+            if (x * x + y * y > R * R)
+            {
+                ++N;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    std::cout << 4 * (Nsqr - N) - 4 * R - 3;
+    return 0;
+}
