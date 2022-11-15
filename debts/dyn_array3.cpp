0db6e55b781270b68bf8ba3eb3abab3754faa8b1
hardius <113363945+hardius@users.noreply.github.com>
2022-11-15 21:56:30 +0300

    Add files via upload

---
+++ debts/dyn_array3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+void snakeArray(int** A, int n)
+{
+    int fill = 1, dd0 = 0, ud0 = n - 1, nn=n*n;
+    for (int i = 0; ;)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            A[j][i] = fill;
+            if (fill == nn) return;
+            ++fill;
+        }
+        ++i;
+        for (int j = n - 1; j >= 0; --j)
+        {
+            A[j][i] = fill;
+            if (fill == nn) return;
+            ++fill;
+        }
+        ++i;
+    }
+}
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    int** a = new int* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        a[i] = new int[n];
+    }
+
+    snakeArray(a, n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            std::cout << a[i][j] << '\t';
+        }
+        std::cout << std::endl;
+    }
+}
